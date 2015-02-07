a908d2dfd100b9e78bc4e6b34f9ff5e302248819
Kishor Mohite <kishormohite888@gmail.com>
2015-02-07 13:54:13 +0530

    Adds method for post request

--- Jasper/Login.cs
+++ Jasper/Login.cs
@@ -13,6 +13,9 @@ namespace Jasper
     {
        public String email, password;
        private String secretKey;
+       public delegate void RESTSuccessCallback(Stream stream);
+       public delegate void RESTErrorCallback(String reason);
+
        Login()
        {
            email = null;
@@ -29,7 +32,11 @@ namespace Jasper
            if(checkEmail()&&checkPassword())
            {
                //Send a HTTP request here
-               
+               string url = "http://kakulkaserver";
+               Dictionary<String, String> post_params=new Dictionary<string,string>();
+               post_params.Add("email",email);
+               post_params.Add("password",password);
+               post(new Uri(url), post_params, null, success_callback, error_callback);
                return 0;
            }
            else
@@ -53,6 +60,78 @@ namespace Jasper
        {
            if (password.Length < 6) return false;
            else return true;
+       }
+
+       void success_callback(Stream response_stream)
+       {
+           byte[] byteArray=new byte[32];
+           response_stream.BeginRead(byteArray,0,32,null,null);
+           secretKey = byteArray.ToString();
+
+       }
+
+       void error_callback(String reason)
+       {
+
+       }
+       public void post(Uri uri, Dictionary<String, String> post_params, Dictionary<String, String> extra_headers, RESTSuccessCallback success_callback, RESTErrorCallback error_callback)
+       {
+           HttpWebRequest request = WebRequest.CreateHttp(uri);
+           request.ContentType = "application/x-www-form-urlencoded";
+           request.Method = "POST";
+           if (extra_headers != null)
+               foreach (String header in extra_headers.Keys)
+                   try
+                   {
+                       request.Headers[header] = extra_headers[header];
+                   }
+                   catch (Exception) { }
+
+
+           //we first obtain an input stream to which to write the body of the HTTP POST
+           request.BeginGetRequestStream((IAsyncResult result) =>
+           {
+               HttpWebRequest preq = result.AsyncState as HttpWebRequest;
+               if (preq != null)
+               {
+                   Stream postStream = preq.EndGetRequestStream(result);
+
+                   //allow for dynamic spec of post body
+                   StringBuilder postParamBuilder = new StringBuilder();
+                   if (post_params != null)
+                       foreach (String key in post_params.Keys)
+                           postParamBuilder.Append(String.Format("{0}={1}&", key, post_params[key]));
+
+                   Byte[] byteArray = Encoding.UTF8.GetBytes(postParamBuilder.ToString());
+
+                   //guess one could just accept a byte[] [via function argument] for arbitrary data types - images, audio,...
+                   postStream.Write(byteArray, 0, byteArray.Length);
+                   postStream.Close();
+
+
+                   //we can then finalize the request...
+                   preq.BeginGetResponse((IAsyncResult final_result) =>
+                   {
+                       HttpWebRequest req = final_result.AsyncState as HttpWebRequest;
+                       if (req != null)
+                       {
+                           try
+                           {
+                               //we call the success callback as long as we get a response stream
+                               WebResponse response = req.EndGetResponse(final_result);
+                               success_callback(response.GetResponseStream());
+                               return;
+                           }
+                           catch (WebException e)
+                           {
+                               //otherwise call the error/failure callback
+                               error_callback(e.Message);
+                               return;
+                           }
+                       }
+                   }, preq);
+               }
+           }, request);
        }
     }
 }
